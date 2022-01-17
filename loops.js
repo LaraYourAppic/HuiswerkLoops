fe2ddb0e5cb24f034002685018b9460d3ad0d09e
LaraYourAppic <Lara@YourAppic.nl>
2022-01-17 15:08:27 +0100

    huiswerkLoops

--- loops.js
+++ loops.js
@@ -5,7 +5,10 @@
 // Hoera!
 // Hoera!
 // ==========================================
-
+const greating ="hoera!"
+for (let i = 0; i < 3; i++) {
+    console.log(greating);
+}
 
 
 // ==========================================
@@ -16,9 +19,13 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
-
-
+// ==========================================
+for (let i = 0; i <= 3; i++){
+    console.log('loop...');
+    if (i === 3 ) {
+        console.log("klaar");
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -33,11 +40,37 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
 
-
+for (let i=0; i<=15; i++) {
+    if (i === 0) {
+        console.log(i + " even");
+    }
+    else if (i % 2 === 0) {
+        console.log(i + " even");
+    }
+    else {
+        console.log(i + " oneven");
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
 // Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
+
+for (let i = 0; i < 10; i++) {
+    if (i <3) {
+        console.log(" " + i);
+    }
+    else if (i <6){
+        console.log(" > " + i);
+    }
+
+    else{
+        console.log(" >> " + i)
+    }
+}
+
+
+
 // Verwachte uitkomsten:
 // 0
 // 1
